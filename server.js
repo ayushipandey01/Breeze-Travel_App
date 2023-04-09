ba4b3e7ab3a269ced9aade9915a4e2cd129b9dfd
Ayushi Pandey <ayushipandey75@gmail.com>
2023-04-10 01:21:31 +0530

    Database connection made & get and post method added

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const hotelRouter = require("./routes/hotel.router");
+const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
+
+const connectDB = require("./config/dbconfig");
 
 const app = express();
+
+app.use(express.json());
+connectDB();
 
 const PORT = 7000;
 
@@ -8,6 +17,14 @@ app.get("/" , (req , res) => {
     res.send("Hello");
 })
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log("Server running on port 7000")
-}) 
+app.use("/api/hotelsdata" , hotelDataAddedToDBRouter);
+
+app.use("/api/hotels", hotelRouter);
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to DB");
+    app.listen(process.env.PORT || PORT, () => {
+        console.log("Server running on port 7000")
+    }) 
+})
+
