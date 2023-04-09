9358fd732c56503d9ff0946c2c6c1fbc886507df
Ayushi Pandey <ayushipandey75@gmail.com>
2023-04-10 01:55:11 +0530

    categoris database added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const hotelRouter = require("./routes/hotel.router");
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
+const categoryDataAddedToDBRouter = require("./routes/categoryimport.router");
+
+const hotelRouter = require("./routes/hotel.router");
+const categoryRouter = require("./routes/category.router");
+
 
 const connectDB = require("./config/dbconfig");
 
@@ -11,15 +15,19 @@ const app = express();
 app.use(express.json());
 connectDB();
 
-const PORT = 7000;
+const PORT = 7500;
 
 app.get("/" , (req , res) => {
     res.send("Hello");
 })
 
-app.use("/api/hotelsdata" , hotelDataAddedToDBRouter);
+app.use("/api/hoteldata" , hotelDataAddedToDBRouter);
+
+app.use("/api/categorydata" , categoryDataAddedToDBRouter);
 
 app.use("/api/hotels", hotelRouter);
+
+app.use("/api/category", categoryRouter);
 
 mongoose.connection.once("open", () => {
     console.log("Connected to DB");
