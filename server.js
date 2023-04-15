1461b29086afcd420459751864040b0b8c1f938f
Ayushi Pandey <ayushipandey75@gmail.com>
2023-04-15 20:19:05 +0530

    cors issue fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ const wishlistRouter = require("./routes/wishlist.router");
 const connectDB = require("./config/dbconfig");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 connectDB();
